e53df30d08dcd81cfd238647adf8ea0773cc793e
selindoga <73192627+selindoga@users.noreply.github.com>
2023-02-17 04:32:25 +0300

    created options menu and levels screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,6 +21,9 @@ public class GameManager : MonoBehaviour
     //Level3_Win = 9,
     //GameOver = 10,
     
+    // yeni sceneler ekledikçe bir sonraki index e koy
+    // yapmayı planladığım levels ekranı indexi 11 olmalı gibi
+    
     public static bool InLevel1;
     public static bool InLevel2;
     public static bool InLevel3;
@@ -33,9 +36,31 @@ public class GameManager : MonoBehaviour
     private String L1Solution = "H";
     private String L2Solution = "HH CX(1,2)";
     private String L3Solution = "IX HH CX(1,2)";
+    
+    private GameObject ImageOfVolume;
+    private GameObject OptionsMenu;
+    private GameObject GameplayParent;
 
+    private static int IndexOfCurrentScene_BeforeGoingTo_LevelsScene;
     private void Start()
     {
+        ImageOfVolume = GameObject.Find("VolumeButton/Image");
+        if (ImageOfVolume != null)
+        {
+            if (AudioListener.volume == 0)
+            {
+                ImageOfVolume.GetComponent<Image>().sprite =
+                    (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Sprites/Buttons/mute.png", typeof(Sprite));
+            } else if (AudioListener.volume > 0)
+            {
+                ImageOfVolume.GetComponent<Image>().sprite =
+                    (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Sprites/Buttons/volume.png", typeof(Sprite));
+            }
+        }
+        
+        OptionsMenu = GameObject.Find("/Canvas/OptionsMenu"); 
+        GameplayParent = GameObject.Find("/Canvas/GameplayParent");
+        
         CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         switch (CurrentSceneIndex)
         {
@@ -54,6 +79,15 @@ public class GameManager : MonoBehaviour
                 InLevel2 = false;
                 InLevel3 = true;
                 break;
+        }
+
+        try
+        {
+            OptionsMenu.SetActive(false);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
         }
     }
 
@@ -104,20 +138,52 @@ public class GameManager : MonoBehaviour
     {
         Application.Quit();
     }
-
+    
     public void ClickVolumeButton()
     {
         if (AudioListener.volume > 0)
         {
             AudioListener.volume = 0;
-            GameObject.Find("VolumeButton/Image").GetComponent<Image>().sprite =
+            ImageOfVolume.GetComponent<Image>().sprite =
                 (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Sprites/Buttons/mute.png", typeof(Sprite));
         }
         else
         {
             AudioListener.volume = 1;
-            GameObject.Find("VolumeButton/Image").GetComponent<Image>().sprite =
+            ImageOfVolume.GetComponent<Image>().sprite =
                 (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Sprites/Buttons/volume.png", typeof(Sprite));
         }
+    }
+
+    public void ClickOptionsButton()
+    {
+        OptionsMenu.SetActive(true);
+        GameplayParent.SetActive(false);
+    }
+
+    public void ClickCloseButton()
+    {       
+        GameplayParent.SetActive(true);
+        OptionsMenu.SetActive(false);
+    }
+
+    public void ClickGoBackToSeeQuestionButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void ClickLevelsButton()
+    {
+        IndexOfCurrentScene_BeforeGoingTo_LevelsScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(11);
+    }
+
+    public void ClickGoBackButton()
+    {
+        SceneManager.LoadScene(IndexOfCurrentScene_BeforeGoingTo_LevelsScene);
+    }
+    public void ClickQuitGameButton()
+    {
+        Application.Quit();
     }
 }
